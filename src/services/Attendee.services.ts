7ceab38d9e907f614a76ad16a8079d1a72e13177
Dheeraj202 <118679239+Dheeraj202@users.noreply.github.com>
2024-07-08 23:39:09 +0530

    add attendee services

---
+++ src/services/Attendee.services.ts
@@ -0,0 +1,16 @@
+import PrismaClient from "../utils/PrismaClient";
+
+class AttendeeServices {
+  public attendees = PrismaClient.meetingAttendee;
+  public createAttendee = async (meetingId: string, userId: string) => {
+    const attendee = await this.attendees.create({
+      data: {
+        joinedAt: new Date(),
+        userId: userId,
+        meetingId: meetingId,
+        meetingsId: meetingId,
+        leftAt: new Date(),
+      },
+    });
+  };
+}
