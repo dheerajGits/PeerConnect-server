ede7520775ec083a817f4f71c8a3351784360a70
Dheeraj202 <118679239+Dheeraj202@users.noreply.github.com>
2024-07-18 14:56:29 +0530

    create attendee controller and add getAllAtendee and remove attendee

---
+++ src/controllers/attendee.controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import AttendeeServices from "../services/Attendee.services";
+
+class AttendeeController {
+  public attendeeServices = new AttendeeServices();
+
+  public getAllLiveAttendees = async (req: Request, res: Response) => {
+    try {
+      const meetingId = req.params.meetingId;
+      if (!meetingId) {
+        res.send(401).send({ message: "meetingId not specified" });
+        return;
+      }
+      const meetingAttendees = await this.attendeeServices.getAllLiveAttendees(
+        meetingId
+      );
+      res.status(202).send({ message: "getAll", data: meetingAttendees });
+    } catch (e) {
+      console.log("..[ERROR IN ATTENDEE CONTROLLER GETLIVEATTENDEE]", e);
+      res.status(400).send({ message: "something went wrong" });
+    }
+  };
+}
